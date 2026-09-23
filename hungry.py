32518dfd52e15a5db742d16cdff5740c27d7001f
Your Name <priyanshm17122003@gmail.com>
2026-09-23 07:06:56 +0530

    burger

--- hungry.py
+++ hungry.py
@@ -2,6 +2,7 @@ hungry = input ("are you hungry ?")
 
 if hungry == 'yes' :
     print("eat samosa")
+    print ("eat biurger ")
 else : 
     thirsty= input("are you thirsty ?")
     if thirsty == "yes" :
